24869d4c49443152382a490c8b4a488fcb757a1f
Chike Chris <cchike2017@gmail.com>
2019-07-24 18:24:46 -0400

    modified form.js file

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,44 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
+export default function Form(props) {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    role: ""
+  });
 
+  function handleChange(event) {
+    const updatedUser = { ...user, [event.target.name]: event.target.value };
+    setUser(updatedUser);
+  }
 
-function Form(props) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.setArrOfTeam([...props.arrOfTeam, user]);
 
-  const [user, setUser] = useState({ name: '', email: '', role: ''})
+    console.log("user: ", user);
+  }
+
   return (
-    <div className='form-container'>
-      <form onSubmit={event => handleSubmit(event)}>
-        <label>
-          Name:
-                    <input
-            type="text"
-            name="name"
-            value={teamMembers.name}
-            onChange={event => handleChange(event)}
-          />
-        </label>
-        <label>
-          Email:
-                    <input
-            type="text"
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>Add Team Member</legend>
+        <div className="form-group row">
+          <label for="name" className="col-sm-2 col-form-label">
+            Name
+            <div className="col-sm-10">
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                placeholder="Enter your name"
+                value={user.name}
+                onChange={handleChange}
+              />
+            </div>
+          </label>
+        </div>
+        <div className="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
             name="email"
-            value={teamMembers.email}
-            onChange={event => handleChange(event)}
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+            value={user.email}
+            onChange={handleChange}
           />
-        </label>
-        <label>
-          Role:
-                    <input
-            type="text"
+        </div>
+        <div className="form-group">
+          <label for="exampleRole1">Role</label>
+          <input
+            type="role"
+            className="form-control"
             name="role"
-            value={teamMembers.role}
-            onChange={event => handleChange(event)}
+            placeholder="Role"
+            onChange={handleChange}
+            value={user.role}
           />
-        </label>
-    
-      </form>
-    </div>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </fieldset>
+    </form>
   );
 }
-
-export default Form;
